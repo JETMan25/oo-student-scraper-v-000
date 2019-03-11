0891e91444da0312e5e056b2a0cba07ef07aa28a
jontenthoff <jontenthoff@hotmail.com>
2019-03-11 17:55:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,8 +11,7 @@ class Scraper
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      profile_path = student.css("a").attribute("href").value
-      student_details[:profile_url] = profile_path # './fixtures/student-site/index.html' + profile_path
+      student_details[:profile_url] = student.css("a").attribute("href").value
       students << student_details
     end
     students
